c3eae1b663aac0e7860ab5d5c3e841193e60738f
Bektursun <samatuulu11@gmail.com>
2019-09-20 12:26:52 +0600

    Created model Product and registered model to admin.py file and created super user

--- source/OnlineMarket/models.py
+++ source/OnlineMarket/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+PRODUCT_OTHER_CHOICE = 'other'
+PRODUCT_CATEGORY_CHOICES = (
+    (PRODUCT_OTHER_CHOICE, 'Другое'),
+    ('food', 'Еда'),
+    ('drink', 'Вода'),
+    ('cloth', 'Одежда'),
+    ('electronics', 'Электроника')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Name')
+    description = models.TextField(max_length=2000, verbose_name='Description', null=True, blank=True)
+    category = models.CharField(max_length=20, verbose_name='Category',
+                                choices=PRODUCT_CATEGORY_CHOICES, default=PRODUCT_OTHER_CHOICE)
+    amount = models.IntegerField(verbose_name='Amount')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Price')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
